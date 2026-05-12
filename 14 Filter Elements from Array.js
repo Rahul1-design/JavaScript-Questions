5132f06700e3577a42eb9b21471087612b828460
Rahul1-design <rahulshrestha3132@gmail.com>
2026-05-12 22:36:27 +0545

    solvin the questions from the leetcode

---
+++ 14 Filter Elements from Array.js
@@ -0,0 +1,36 @@
+// Question from the leetCode
+
+// Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
+// The fn function takes one or two arguments:
+// arr[i] - number from the arr
+// i - index of arr[i]
+// filteredArr should only contain the elements from the arr for which the expression fn(arr[i], i) evaluates to a truthy value. A truthy value is a value where Boolean(value) returns true.
+// Please solve it without the built-in Array.filter method.
+// Example 1:
+// Input: arr = [0,10,20,30], fn = function greaterThan10(n) { return n > 10; }
+// Output: [20,30]
+// Explanation:
+// const newArray = filter(arr, fn); // [20, 30]
+// The function filters out values that are not greater than 10
+
+// TODO just tried from the filter method
+// const filterArray = (arr) => {
+//   return arr.filter((item) => item > 10);
+// };
+// console.log(filterArray([10, 20, 30]));
+
+let filter = function (arr, fn) {
+  let filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      filteredArr.push(arr[i]);
+    }
+  }
+  return filteredArr;
+};
+
+console.log(
+  filter([10, 20, 30], function (n) {
+    return n > 10;
+  })
+);
